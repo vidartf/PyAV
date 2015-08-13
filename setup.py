8a4178a38b8226b092c98e73f6447b1e0c2aa74d
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-08-13 16:36:03 +0200

    Unsilence config compiler

--- setup.py
+++ setup.py
@@ -161,7 +161,7 @@ def new_compiler(*args, **kwargs):
 
     """
     cc = _new_compiler(*args, **kwargs)
-    if kwargs.pop('silent', True):
+    if kwargs.pop('silent', False):
         cc.spawn = _CCompiler_spawn_silent
     return cc
 
